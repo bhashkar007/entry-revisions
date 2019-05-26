ffe3a8868070c557a1f7b79856c51d72179d5af4
Bhashkar Yadav <bhashkar007@gmail.com>
2019-05-26 18:56:39 +0530

    plugin files

---
+++ entry_revisions/addon.setup.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+	'name'           => 'Entry Revisions',
+	'version'        => 'Bhashkar Yadav',
+	'author'         => 'http://360adaptive.com',
+	'author_url'     => 'http://360adaptive.com/',
+	'description'    => 'This plugin can be used to get entry version data',
+	'namespace'      => '360Adaptive\EntryRevisions',
+	'docs_url'       => 'http://360adaptive.com/entry-revisions-plugin/',
+	'settings_exist' => TRUE
+);
